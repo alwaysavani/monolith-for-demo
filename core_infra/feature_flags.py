ab9ee40c3c56e6c35199f37f2d495f38daa7f602
Avani <avani0698@gmail.com>
2026-06-08 20:51:48 +0530

    Disable legacy billing feature toggle
    Comment out the is_legacy_billing_enabled function.

--- core_infra/feature_flags.py
+++ core_infra/feature_flags.py
@@ -1,6 +1,6 @@
-def is_legacy_billing_enabled():
-    """Platform Team owns this toggle."""
-    return True
+# def is_legacy_billing_enabled():
+#     """Platform Team owns this toggle."""
+#     return True
 
 def get_max_db_connections():
     return 100
